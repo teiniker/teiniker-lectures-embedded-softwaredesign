6c41b855fc5c47defda57658b88cbc8f1e85d47b
teiniker <egon.teiniker@gmail.com>
2020-03-02 20:19:59 +0100

    Added test cases.

--- configuration-management/testing/compare/test/compare_test.c
+++ configuration-management/testing/compare/test/compare_test.c
@@ -1,6 +1,8 @@
 #include <unity.h>
 #include <stdio.h>
 
+#include <compare.h>
+
 void setUp(void)
 {
 	// setup code
@@ -11,21 +13,51 @@ void tearDown(void)
 	// tear down code
 }
 
-void test_A(void)
+void test_A_greater_B(void)
 {
-	TEST_ASSERT_EQUAL_HEX8(40, 40);
+	// Setup
+	int a = 17;
+	int b = 13;
+	
+	// Exercise
+	int result = compare(a,b);
+	
+	// Verify
+	TEST_ASSERT_EQUAL(1, result);
 }
 
-void test_B(void)
+void test_A_lessthan_B(void)
 {
-	TEST_ASSERT_EQUAL_HEX8(80, 90);
+	// Setup
+	int a = 17;
+	int b = 21;
+	
+	// Exercise
+	int result = compare(a,b);
+	
+	// Verify
+	TEST_ASSERT_EQUAL(-1, result);
+}
+
+void test_A_equal_B(void)
+{
+	// Setup
+	int a = 23;
+	int b = 23;
+	
+	// Exercise
+	int result = compare(a,b);
+	
+	// Verify
+	TEST_ASSERT_EQUAL(0, result);
 }
 
 int main(void)
 {
 	UNITY_BEGIN();
-	RUN_TEST(test_A);
-	RUN_TEST(test_B);
+	RUN_TEST(test_A_greater_B);
+	RUN_TEST(test_A_lessthan_B);
+	RUN_TEST(test_A_equal_B);	
 	return UNITY_END();
 }
 
