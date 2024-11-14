11add471fdbea194867530b526c4d638502bf07d
teiniker <egon.teiniker@gmail.com>
2024-11-14 22:19:24 +0100

    Refactoring.

--- programming-c++/oop/classes/can-message/can-msg.h
+++ programming-c++/oop/classes/can-message/can-msg.h
@@ -6,6 +6,11 @@
 
 class CanMsg
 {
+    private:
+        uint32_t _id;
+        uint8_t _dlc;
+        uint8_t _data[8];
+
     public:
         CanMsg(uint32_t id, uint8_t dlc, const uint8_t *data);
         
@@ -16,11 +21,6 @@ class CanMsg
 
         // Methods
         std::string asString() const;
-
-    private:
-        uint32_t _id;
-        uint8_t _dlc;
-        uint8_t _data[8];
 };
 
 #endif 
