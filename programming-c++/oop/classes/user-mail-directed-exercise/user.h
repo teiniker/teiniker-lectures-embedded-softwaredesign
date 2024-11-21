2ad879cd5e5a79377331b9b69b5ea5d08d44f299
teiniker <egon.teiniker@gmail.com>
2024-11-21 20:22:58 +0100

    Added exercise.

---
+++ programming-c++/oop/classes/user-mail-directed-exercise/user.h
@@ -0,0 +1,28 @@
+#ifndef _USER_H_
+#define _USER_H_
+
+#include <string>
+#include <vector>
+#include "mail.h"
+
+class User 
+{
+	private:
+		int _id; 
+		std::string _username;
+		std::string _password;
+ 
+	public:                
+		User(const int id, const std::string& username, const std::string& password);
+
+		int id(void) const;
+		void id(const int id);
+		
+		std::string username(void) const;
+		void username(const std::string& username);
+		
+		std::string password() const;
+		void password(const std::string& password);
+};
+
+#endif /*_USER_H_ */
