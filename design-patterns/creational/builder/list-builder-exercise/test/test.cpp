e9ad06c493dd3d2a076e04b22be8183162fe2584
teiniker <egon.teiniker@gmail.com>
2025-01-13 21:03:00 +0100

    Added exercise.

---
+++ design-patterns/creational/builder/list-builder-exercise/test/test.cpp
@@ -0,0 +1,38 @@
+#include <gtest/gtest.h>
+
+#include <list-builder-json.h>
+#include <list-builder-xml.h>
+
+using namespace std;
+
+TEST(BuilderTest, ListBuilderJson)
+{
+    // Setup
+    ListBuilderJson builder;
+    builder.item("one").item("two").item("three");
+    
+    // Exercise
+    // The result of concrete builders are usually different types, 
+    // so we have to ask the concrete builder for its result.
+    string json = builder.toJson();
+
+    // Verify
+    cout << json << endl;
+    ASSERT_EQ("{\"list\":[\"one\",\"two\",\"three\"]}", json);
+}
+
+
+TEST(BuilderTest, ListBuilderXml)
+{
+    // Setup
+    ListBuilderXml builder;
+    builder.item("one").item("two").item("three");
+    
+    // Exercise
+    string xml = builder.toXml();
+
+    // Verify
+    cout << xml << endl;
+    ASSERT_EQ("<list><item>one</item><item>two</item><item>three</item></list>", xml);
+}
+
