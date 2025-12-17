070b1b53d5e61a4c48dc51934257d64e4e4d5c97
teiniker <egon.teiniker@gmail.com>
2025-12-17 11:38:07 +0100

    Added test case.

--- design-patterns/creational/builder/list-builder-exercise/test/test.cpp
+++ design-patterns/creational/builder/list-builder-exercise/test/test.cpp
@@ -36,3 +36,18 @@ TEST(BuilderTest, ListBuilderXml)
     ASSERT_EQ("<list><item>one</item><item>two</item><item>three</item></list>", xml);
 }
 
+TEST(BuilderTest, BaseType)
+{
+    // Setup
+    unique_ptr<ListBuilder> builder = make_unique<ListBuilderJson>();
+    
+    // Exercise
+    builder->item("alpha").item("beta").item("gamma");
+
+    // Verify
+    // Downcast to the concrete type to get the result
+    ListBuilderJson* jsonBuilder = dynamic_cast<ListBuilderJson*>(builder.get());
+    ASSERT_NE(nullptr, jsonBuilder); // Verify the downcast worked
+    string json = jsonBuilder->toJson();
+    ASSERT_EQ("{\"list\":[\"alpha\",\"beta\",\"gamma\"]}", json);
+}
